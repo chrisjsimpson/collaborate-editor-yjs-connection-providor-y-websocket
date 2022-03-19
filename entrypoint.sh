6f10d20bf0ce12d17181d0613f40890ebe15f25f
chrisjsimpson <chris.j.simpson@live.co.uk>
2022-03-19 22:36:13 +0000

    set entrypoint cwd

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 
-exec PORT=1234 YPERSISTENCE=./dbDir node ./node_modules/y-websocket/bin/server.js
+set -x
+cd /usr/src/app
+PORT=1234 YPERSISTENCE=./dbDir node ./node_modules/y-websocket/bin/server.js
 
